914f6734685dac3a07c189f581cf875a0dad9fff
BHAAGYA <bhagyashreedas2002@gmail.com>
2024-12-24 17:21:41 +0530

    Api_cards

--- src/constants/url.js
+++ src/constants/url.js
@@ -3,4 +3,7 @@
 // export const BASE_URL = 'http://192.168.29.138:8000/api/v1/';
 // export const BAS_URL = 'http://192.168.29.138:8000/';
 export const BASE_URL = 'http://192.168.0.111:8000/api/v1/';
+
 export const BAS_URL = 'http://192.168.0.111:8000/';
+// export const BAS_URL = 'https://tatapower.epsumlabs.in/';
+// export const BASE_URL = 'https://tatapower.epsumlabs.in/api/v1/';
